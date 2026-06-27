4ea0e7db75970c9266821628cd37869415870dc4
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-06-27 08:35:10 +0530

    Time: 540 ms (17.69%), Space: 135 MB (16.46%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,41 +1,38 @@
 import java.util.PriorityQueue;
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-
-        PriorityQueue<pair> kk=new PriorityQueue <> (
-            (a,b)->{
-                return Double.compare(a.ans,b.ans);
-            }
+        PriorityQueue<pair> kk=new PriorityQueue<>(
+        (a,b)->{
+            return Double.compare(a.val,b.val);
+        }
         );
-
 
         for(int i=0;i<arr.length;i++){
             for(int j=i+1;j<arr.length;j++){
-
                 kk.add(new pair(arr[i],arr[j],(double)arr[i]/arr[j]));
-
             }
         }
 
-
         for(int i=1;i<k;i++){
-            kk.poll();
+            pair l=kk.poll();
 
         }
 
-        pair l=kk.poll();
-        return new int[]{l.val1,l.val2};
+        return new int[]{kk.peek().ii,kk.peek().jj};
+
+
+    
         
     }
 }
 class pair{
-    int val1;
-    int val2;
-    double ans;
+    int ii;
+    int jj;
+    double val;
 
     pair(int a,int b,double c){
-     val1=a;
-        val2=b;
-        ans=c;
+        ii=a;
+        jj=b;
+        val=c;
     }
 }
