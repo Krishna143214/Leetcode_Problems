46dcf573c00ff7ffcfdf76163bf408f6b82d7132
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-07-27 21:38:25 +0530

    Time: 16 ms (38.43%), Space: 47.5 MB (66.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -3,61 +3,61 @@ import java.util.PriorityQueue;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-HashMap<Integer,Integer> kk=new HashMap<>();
-
-for(int i=0;i<nums.length;i++){
-    if(kk.containsKey(nums[i])){
-        kk.put(nums[i],kk.get(nums[i])+1);
+PriorityQueue<pair> kk=new PriorityQueue<>(
+(a,b)->{
+    if(a.freq!=b.freq){
+        return b.freq-a.freq;
     }
-    else{
-        kk.put(nums[i],1);
-    }
+    return a.val-b.val;
 }
 
-PriorityQueue<pair> ll=new PriorityQueue<>(
-    (a,b)->{
-        if(a.freq!=b.freq){
-            return b.freq-a.freq;
-        }
-        return a.val-b.val;
-    }
+
 );
 
-for(Map.Entry<Integer,Integer> nn:kk.entrySet()){
-    ll.add(new pair(nn.getKey(),nn.getValue()));
+HashMap<Integer,Integer> ll=new HashMap<>();
+
+for(int i=0;i<nums.length;i++){
+    if(ll.containsKey(nums[i])){
+        ll.put(nums[i],ll.get(nums[i])+1);
+    }
+    else{
+        ll.put(nums[i],1);
+    }
 }
 
 
-int []p=new int[k];
+for(Map.Entry<Integer,Integer> ss:ll.entrySet()){
+
+    kk.add(new pair(ss.getKey(),ss.getValue()));
+
+}
+
+
+int res[]=new int[k];
 
 for(int i=0;i<k;i++){
-    pair t=ll.poll();
-    p[i]=t.val;
+    pair f=kk.poll();
+    res[i]=f.val;
+
 }
-return p;
 
 
-
-
-
-
-
-
-
+return res;
 
 
     }
 
 }
+
 class pair{
     int val;
     int freq;
-
     pair(int a,int b){
         val=a;
         freq=b;
-
     }
 }
+
+
 
 
