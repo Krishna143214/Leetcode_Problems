2a07c60575019249f003371f8f390ec70f7236db
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-07-28 01:00:17 +0530

    Time: 232 ms (39.43%), Space: 105.5 MB (55.56%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.java
@@ -4,31 +4,30 @@ import java.util.Collections;
 class Solution {
     public long maxKelements(int[] nums, int k) {
 
-        long res=0;
-        PriorityQueue<Integer> kk=new PriorityQueue<>(Collections.reverseOrder());
+   PriorityQueue<Integer> kk=new PriorityQueue<>(Collections.reverseOrder());
 
-        for(int i=0;i<nums.length;i++){
-            kk.add(nums[i]);
-        }
+   for(int i=0;i<nums.length;i++){
+    kk.add(nums[i]);
+   }
+ long ans=0;
+   for(int i=0;i<k;i++){
+    int v=kk.poll();
+    ans=ans+v;
+    int r=v/3;
 
-        for(int i=0;i<k;i++){
-            int val=kk.poll();
-            res=res+val;
+    if(v%3!=0){
+        r++;
+    }
+    kk.add(r);
 
-           if(val%3==0){
-            kk.add(val/3);
-           }
-           else{
-           kk.add((val/3)+1);
-           }
-        }
-
-return res;
+   }
 
 
 
 
 
+
+return ans;
 
 
         
