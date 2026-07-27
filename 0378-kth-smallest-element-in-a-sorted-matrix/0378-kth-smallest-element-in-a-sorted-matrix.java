f988cf6ae602449b364eee56c548788abc9359dd
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-07-27 21:41:35 +0530

    Time: 25 ms (20.64%), Space: 50.7 MB (76.77%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -6,16 +6,15 @@ class Solution {
 
         PriorityQueue<Integer> kk=new PriorityQueue<>();
 
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                kk.add(matrix[i][j]);
-            }
+       for(int i=0;i<matrix.length;i++){
+        for(int j=0;j<matrix[i].length;j++){
+            kk.add(matrix[i][j]);
         }
+       }
 
 
-       for(int i=1;i<k;i++){
+       for(int i=0;i<k-1;i++){
         kk.poll();
-
        }
        return kk.peek();
  
