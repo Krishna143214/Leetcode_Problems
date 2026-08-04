e21ed36087c975d618229ab88814206b2f827383
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-08-05 01:18:12 +0530

    Time: 32 ms (14.47%), Space: 48.2 MB (7.73%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -1,48 +1,59 @@
 import java.util.Stack;
+import java.util.HashSet;
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        Stack<pair> kk=new Stack<>();
-        StringBuilder ll=new StringBuilder(s);
 
-        for(int i=0;i<s.length();i++){
-          if(s.charAt(i)=='('||s.charAt(i)==')'){
-            if(kk.isEmpty()){
-                kk.push(new pair(s.charAt(i),i));
-            }
-            else if(kk.peek().val=='('&& s.charAt(i)==')'){
-                kk.pop();
-            }
-            else{
-                kk.push(new pair(s.charAt(i),i));
-            }
-          }
+Stack<pair> kk=new Stack<>();
+
+for(int i=0;i<s.length();i++){
+    if(s.charAt(i)=='('||s.charAt(i)==')'){
+        if(kk.isEmpty()){
+            kk.push(new pair(s.charAt(i),i));
         }
+        else if(kk.peek().val=='('&&s.charAt(i)==')'){
+            kk.pop();
+        }
+        else{
+            kk.push(new pair(s.charAt(i),i));
+        }
+    }
 
-
-while(!kk.isEmpty()){
-    pair b=kk.pop();
-
-    ll.deleteCharAt(b.indi);
 
 }
 
-return ll.toString();
+HashSet<Integer> d=new HashSet<>();
+StringBuilder oo=new StringBuilder();
+
+while(!kk.isEmpty()){
+    d.add(kk.peek().ind);
+    kk.pop();
+
+}
+
+for(int k=0;k<s.length();k++){
+    if(d.contains(k)){
+        continue;
+    }
+    else{
+        oo.append(s.charAt(k));
+    }
+}
 
 
 
-
-
+return oo.toString();
 
 
         
     }
 }
+
 class pair{
     char val;
-    int indi;
+    int ind;
 
     pair(char a,int b){
         val=a;
-        indi=b;
+        ind=b;
     }
 }
