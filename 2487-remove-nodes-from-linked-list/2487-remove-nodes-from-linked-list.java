a84e5a6d7fa84fb493c3fe8302ed5577a553855e
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-08-08 21:21:45 +0530

    Time: 92 ms (5.53%), Space: 140.5 MB (10.34%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -12,57 +12,63 @@
  import java.util.Stack;
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        if(head.next==null){
-            return head;
-        }
+  if(head.next==null){
+    return head;
+  }
 
-        Stack<Integer> kk=new Stack<>();
 
-        ListNode t=head;
+  Stack<Integer> kk=new Stack<>();
+  ListNode t=head;
 
-        while(t!=null){
-            if(kk.isEmpty()){
-                kk.push(t.val);
+  while(t!=null){
+    if(kk.isEmpty()){
+        kk.push(t.val);
+    }
 
-            }
-            else if(kk.peek()<t.val){
-                while(!kk.isEmpty()&&kk.peek()<t.val){
-                    kk.pop();
-                }
-                kk.push(t.val);
-            }
+    else if(kk.peek()<t.val){
+        while(!kk.isEmpty()&&kk.peek()<t.val){
+            kk.pop();
 
-            else{
-                kk.push(t.val);
-            }
-            t=t.next;
 
         }
 
+      
+            kk.push(t.val);
+        
+    }
 
-        if(kk.isEmpty()){
-            return null;
-        }
-        else if(kk.size()==1){
-            return new ListNode(kk.pop());
-        }
+    else{
+             kk.push(t.val);
 
-        Stack<Integer> jj=new Stack<>();
-        while(!kk.isEmpty()){
-            jj.push(kk.pop());
-        }
-
-        ListNode h2=new ListNode(jj.pop());
-        ListNode t2=h2;
-
-        while(!jj.isEmpty()){
-            ListNode s=new ListNode(jj.pop());
-            t2.next=s;
-            t2=t2.next;
-        }
+    }
+    t=t.next;
+  }
 
 
-        return h2;
+
+  if(kk.isEmpty()){
+    return null;
+  }
+    Stack<Integer> kj=new Stack<>();
+
+  while(!kk.isEmpty()){
+    kj.push(kk.pop());
+  }
+
+
+ ListNode h2=new ListNode(kj.pop());
+ ListNode t2=h2;
+
+ while(!kj.isEmpty()){
+    ListNode f=new ListNode(kj.pop());
+    t2.next=f;
+    t2=t2.next;
+ }
+
+
+ return h2;
+
+
         
     }
 }
