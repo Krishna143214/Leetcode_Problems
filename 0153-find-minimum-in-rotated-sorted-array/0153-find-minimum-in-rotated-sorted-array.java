2998b8e5323d36554fe62756960a5b8e6493a17a
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-09 21:53:38 +0530

    Time: 0 ms (100%), Space: 43.8 MB (55.27%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -7,18 +7,20 @@ int k=0;
 while(i<=j){
     int mid=(i+j)/2;
 
+
     if(nums[mid]>nums[nums.length-1]){
         i=mid+1;
     }
     else{
         k=mid;
         j=mid-1;
-
     }
 }
 
-
 return nums[k];
+
+
+
 
         
 
