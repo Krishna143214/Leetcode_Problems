907e7250fb9c616bdda84f10d74de1a0c3c8c3d6
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-13 23:13:08 +0530

    Time: 26 ms (30.55%), Space: 49.2 MB (96.44%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -17,28 +17,33 @@ if(head.next==null){
     return new int[]{0};
 }
 
+ListNode t=head;
 int arr[]=new int[10000];
 int k=0;
-ListNode t=head;
 
 while(t!=null){
     arr[k]=t.val;
-    t=t.next;
     k++;
+    t=t.next;
 }
 
-Stack<Integer> kk=new Stack<>();
 
 int res[]=new int[k];
 
+Stack<Integer> kk=new Stack<>();
 for(int i=k-1;i>=0;i--){
     if(kk.isEmpty()){
-        kk.push(arr[i]);
         res[i]=0;
+        kk.push(arr[i]);
     }
 
-    else if(kk.peek()<=arr[i]){
-        while(!kk.isEmpty()&&kk.peek()<=arr[i]){
+    else if(kk.peek()>arr[i]){
+        res[i]=kk.peek();
+        kk.push(arr[i]);
+    }
+
+    else{
+        while(!kk.isEmpty() && kk.peek()<=arr[i]){
             kk.pop();
         }
 
@@ -49,14 +54,9 @@ for(int i=k-1;i>=0;i--){
         else{
             res[i]=kk.peek();
             kk.push(arr[i]);
-
         }
     }
 
-    else{
-        res[i]=kk.peek();
-            kk.push(arr[i]); 
-    }
 }
 
 return res;
