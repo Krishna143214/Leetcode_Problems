a5a3f364399f03f4ca251609f9f70b9d922bf781
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-13 07:27:29 +0530

    Time: 0 ms (100%), Space: 43.2 MB (53.78%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,23 +1,30 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
+
 class Solution {
-    public boolean stoneGame(int[] piles) {
+    public boolean stoneGame(int[] nums) {
 
-        PriorityQueue<Integer> kk=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<piles.length;i++){
-            kk.add(piles[i]);
-        }
-        int ca=0;
-        int cb=0;
+        int i=0;
+        int j=nums.length-1;
+        int a=0;
+        int b=0;
 
-        while(!kk.isEmpty()){
-            ca=ca+kk.poll();
-            cb=cb+kk.poll();
-        }
+        while(i<j){
+            if(nums[i]>nums[j]){
+                a=a+nums[i];
+                b=b+nums[j];
+            }
+            else{
+                a=a+nums[j];
+                b=b+nums[i];
 
-        if(ca>cb){
-            return true;
-        }
-        return false;
+            }
+            i++;
+            j--;        }
+
+            if(a>b){
+                return true;
+            }
+            return false;
+
+        
     }
 }
