3d905e531572b8b85242944d592f618e18809fb2
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-14 00:15:48 +0530

    Time: 29 ms (14.36%), Space: 47.4 MB (38.85%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -3,57 +3,59 @@ import java.util.HashSet;
 class Solution {
     public String minRemoveToMakeValid(String s) {
 
-Stack<pair> kk=new Stack<>();
+        Stack<pair> kk=new Stack<>();
 
-for(int i=0;i<s.length();i++){
-    if(s.charAt(i)=='('||s.charAt(i)==')'){
-        if(kk.isEmpty()){
-            kk.push(new pair(s.charAt(i),i));
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)==')'){
+                if(kk.isEmpty()){
+                    kk.push(new pair(s.charAt(i),i));
+                }
+                else if(kk.peek().val=='('&&s.charAt(i)==')'){
+                    kk.pop();
+                }
+                else{
+
+                           kk.push(new pair(s.charAt(i),i));
+                }
+            }
         }
-        else if(kk.peek().val=='('&&s.charAt(i)==')'){
-            kk.pop();
+
+        char a[]=s.toCharArray();
+
+        while(!kk.isEmpty()){
+            pair n=kk.pop();
+            a[n.ind]='*';
         }
-        else{
-            kk.push(new pair(s.charAt(i),i));
+
+        StringBuilder res=new StringBuilder();
+
+        for(int i=0;i<a.length;i++){
+            if(a[i]=='*'){
+                continue;
+            }
+            else{
+                res.append(a[i]);
+            }
         }
-    }
 
 
-}
-
-HashSet<Integer> d=new HashSet<>();
-StringBuilder oo=new StringBuilder();
-
-while(!kk.isEmpty()){
-    d.add(kk.peek().ind);
-    kk.pop();
-
-}
-
-for(int k=0;k<s.length();k++){
-    if(d.contains(k)){
-        continue;
-    }
-    else{
-        oo.append(s.charAt(k));
-    }
-}
+        return res.toString();
 
 
-
-return oo.toString();
 
 
         
     }
 }
-
 class pair{
     char val;
     int ind;
 
-    pair(char a,int b){
+    pair(char a, int b){
         val=a;
         ind=b;
     }
 }
+
+
+
