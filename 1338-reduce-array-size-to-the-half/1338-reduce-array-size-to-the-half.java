745dae1fa1ad4349fa0258a239818667f27c42f2
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-14 02:42:39 +0530

    Time: 62 ms (5.1%), Space: 93.1 MB (14.28%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,38 +1,66 @@
 import java.util.HashMap;
 
 import java.util.PriorityQueue;
-import java.util.Collections;
+
 
 class Solution {
     public int minSetSize(int[] arr) {
-   HashMap<Integer,Integer> kk=new HashMap<>();
 
-   for(int i=0;i<arr.length;i++){
-    if(kk.containsKey(arr[i])){
-        kk.put(arr[i],kk.get(arr[i])+1);
-    }
-    else{
-        kk.put(arr[i],1);
-    }
-   }
+        HashMap<Integer,Integer> jj=new HashMap<>();
 
-   PriorityQueue<Integer> oo=new PriorityQueue<>(Collections.reverseOrder());
-  int s=arr.length;
-   for(Map.Entry<Integer,Integer> ll:kk.entrySet()){
-    oo.add(ll.getValue());
+        for(int i=0;i<arr.length;i++){
+            if(jj.containsKey(arr[i])){
+jj.put(arr[i],jj.get(arr[i])+1);
+            }
 
-   }
-   int c=0;
+            else{
+                jj.put(arr[i],1);
+            }
+        }
 
-   while(s>arr.length/2){
-    s=s-oo.poll();
-    c++;
-   }
 
-   return c;
+        PriorityQueue<pair> kk=new PriorityQueue<>(
+            (a,b)->{
+                if(a.freq!=b.freq){
+                    return b.freq-a.freq;
+                }
 
+                return a.val-b.val;
+            }
+        );
+
+
+
+
+for(Map.Entry<Integer,Integer> nn:jj.entrySet()){
+kk.add(new pair(nn.getKey(),nn.getValue()));
+}
+
+int c=arr.length/2;
+
+int k=arr.length;
+int res=0;
+
+
+while(k>c){
+    pair a=kk.poll();
+    k=k-a.freq;
+    res++;
+
+}
+
+return res;
 
         
+    }
+}
+
+class pair{
+    int val;
+    int freq;
+    pair(int a,int b){
+        val=a;
+        freq=b;
     }
 }
 
