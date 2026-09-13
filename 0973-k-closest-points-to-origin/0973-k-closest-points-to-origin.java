fdef0936914428fa1059c8d0e1fcb53d7e5bfac6
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-13 22:54:43 +0530

    Time: 24 ms (82.52%), Space: 56 MB (33.89%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -2,27 +2,30 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int[][] kClosest(int[][] poi, int k) {
-        PriorityQueue<pair> ll=new PriorityQueue<>(
-            (a,b)->{
-                return a.diff-b.diff;
 
+        PriorityQueue<pair> kk=new PriorityQueue<>(
+            (a,b)->{
+                       if(a.dif!=b.dif){
+                        return a.dif-b.dif;
+                       }
+
+                       return a.fir-b.fir;
             }
         );
-
+        
         for(int i=0;i<poi.length;i++){
-            ll.add(new pair(poi[i][0],poi[i][1],poi[i][0]*poi[i][0]+poi[i][1]*poi[i][1]));
+            kk.add(new pair(poi[i][0],poi[i][1],poi[i][0]*poi[i][0]+poi[i][1]*poi[i][1]));
         }
 
 
-        int ans[][]=new int[k][2];
-
+int res[][]=new int[k][2];
         for(int i=0;i<k;i++){
-            pair t=ll.poll();
-            ans[i][0]=t.fir;
-            ans[i][1]=t.sec;
-        }
+            pair f=kk.poll();
+res[i][0]=f.fir;
+res[i][1]=f.sec;
 
-        return ans;
+        }
+return res;
 
 
 
@@ -30,13 +33,13 @@ class Solution {
 }
 
 class pair{
-    int fir;
-    int sec;
-    int diff;
+int fir;
+int sec;
+int dif;
 
-    pair(int a,int b,int c){
-        fir=a;
-        sec=b;
-        diff=c;
-    }
+pair(int a,int b,int c){
+    fir=a;
+    sec=b;
+    dif=c;
+}
 }
