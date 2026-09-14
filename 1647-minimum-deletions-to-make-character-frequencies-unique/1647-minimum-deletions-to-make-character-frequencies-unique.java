1eecb73793a3f96ad4492a86372cda68c1f9e70e
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-14 18:15:47 +0530

    Time: 79 ms (5.01%), Space: 47.7 MB (48.03%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,47 +1,43 @@
 import java.util.HashMap;
 class Solution {
     public int minDeletions(String s) {
-        int c=0;
-        HashMap<Character,Integer> kk=new HashMap<>();
-        HashMap<Integer,Integer> k=new HashMap<>();
+  HashMap<Character,Integer> kk=new HashMap<>();
 
-        for(int i=0;i<s.length();i++){
-            if(kk.containsKey(s.charAt(i))){
-                kk.put(s.charAt(i),kk.get(s.charAt(i))+1);
-            }
-            else{
-                kk.put(s.charAt(i),1);
-            }
+  for(int i=0;i<s.length();i++){
+    if(kk.containsKey(s.charAt(i))){
+        kk.put(s.charAt(i),kk.get(s.charAt(i))+1);
+    }
+    else{
+        kk.put(s.charAt(i),1);
+    }
+  }
+int c=0;
+
+  HashSet<Integer> ss=new HashSet<>();
+
+  for(Map.Entry<Character,Integer> nn:kk.entrySet()){
+
+    if(ss.contains(nn.getValue())){
+
+        int k=nn.getValue();
+        while(k>0&&ss.contains(k)){
+            k--;
+            c++;
         }
 
-        for(Map.Entry<Character,Integer> ll:kk.entrySet()){
-            if(k.containsKey(ll.getValue())){
-            int j=ll.getValue();
-
-            while(k.containsKey(j)&&j>0){
-                j=j-1;
-                c++;
-            }
-            if(j==0){
-
-            }
-            else{
-                k.put(j,1);
-            }
-
-                
-              
-            
-                   
-                
-            }
-            else{
-                k.put(ll.getValue(),1);
-            }
-
+        if(k>0){
+            ss.add(k);
         }
 
-        return c;
+    }
+
+    else{
+        ss.add(nn.getValue());
+    }
+ 
+  }
+
+  return c;
 
         
     }
