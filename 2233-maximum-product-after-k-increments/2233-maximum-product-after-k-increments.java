f4d0d7d0e3a5c65fc6ce963d9d9c6822f6dd30d9
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-15 23:44:00 +0530

    Time: 305 ms (84.15%), Space: 91.2 MB (90.21%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.java
@@ -2,28 +2,27 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int maximumProduct(int[] nums, int k) {
-   PriorityQueue<Integer> kk=new PriorityQueue<>();
+PriorityQueue<Integer> kk=new PriorityQueue<>();
 
-   for(int i=0;i<nums.length;i++){
+for(int i=0;i<nums.length;i++){
     kk.add(nums[i]);
-   }
+}
 
 
-  for(int i=0;i<k;i++){
+for(int i=0;i<k;i++){
+    int a=kk.poll();
+    kk.add(a+1);
+}
 
-int f=(kk.poll()+1);
-    kk.add(f);
 
-  }
+long res=1;
 
-  long ans=1;
+while(!kk.isEmpty()){
 
-  while(!kk.isEmpty()){
-    int p=kk.poll();
-    ans=(ans*p)%1000000007;
-  }
+    res=(res*kk.poll())%1000000007;
+}
 
-  return (int)ans;
+return (int)res;
 
         
     }
