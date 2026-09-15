e51d8036a2471f13410434256409ccfe57a2b721
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-15 23:54:52 +0530

    Time: 47 ms (6.07%), Space: 114.6 MB (10.06%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -3,44 +3,40 @@ import java.util.HashMap;
 class Solution {
     public int minimumRounds(int[] tasks) {
 
-        HashMap<Integer,Integer> kk=new HashMap<>();
+    HashMap<Integer,Integer> kk=new HashMap<>();
 
-        for(int i=0;i<tasks.length;i++){
-            if(kk.containsKey(tasks[i])){
-                kk.put(tasks[i],kk.get(tasks[i])+1);
-            }
-            else{
-                kk.put(tasks[i],1);
-            }
+
+    for(int i=0;i<tasks.length;i++){
+        if(kk.containsKey(tasks[i])){
+            kk.put(tasks[i],kk.get(tasks[i])+1);
         }
- int count=0;
-        for(Map.Entry<Integer,Integer> ll:kk.entrySet()){
-                if(ll.getValue()==1){
-                        return -1;
-                    }
+        else{
+            kk.put(tasks[i],1);
+        }
+    }
 
-                while(ll.getValue()>0){
-                    if(ll.getValue()==2){
-                        kk.put(ll.getKey(),ll.getValue()-2);
-                        count++;
-                    }
-                    else{
-                        kk.put(ll.getKey(),ll.getValue()-3);
-                        count++;
-                    }
-                  
-                
-                }
 
-               
+    int c=0;
 
-            
-           
+    for(Map.Entry<Integer,Integer> ll:kk.entrySet()){
+        if(ll.getValue()<=1){
+            return -1;
         }
 
 
+while(ll.getValue()>0){
+         if(ll.getValue()==2){
+          kk.put(ll.getKey(),ll.getValue()-2);
+            c++;
+        }
+        else{
+            kk.put(ll.getKey(),ll.getValue()-3);
+            c++;
+        }}
+    }
 
-            return count;
+
+    return c;
         
     }
 }
