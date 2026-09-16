2972e245ed0f14588c587a51a0f3f90de4ddc74b
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-17 00:23:11 +0530

    Time: 83 ms (5.31%), Space: 138.4 MB (12.19%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -12,61 +12,93 @@
  import java.util.Stack;
 class Solution {
     public ListNode removeNodes(ListNode head) {
-  if(head.next==null){
+if(head.next==null){
     return head;
-  }
+}
+
+ListNode t=head;
+int arr[]=new int[100000];
+int c=0;
 
 
-  Stack<Integer> kk=new Stack<>();
-  ListNode t=head;
+while(t!=null){
 
-  while(t!=null){
+arr[c]=t.val;
+t=t.next;
+c++;
+
+}
+
+Stack<Integer> kk=new Stack<>();
+int res[]=new int[c];
+
+
+
+for(int i=0;i<c;i++){
+
     if(kk.isEmpty()){
-        kk.push(t.val);
+      
+    
+        kk.push(arr[i]);
+
     }
 
-    else if(kk.peek()<t.val){
-        while(!kk.isEmpty()&&kk.peek()<t.val){
+    else if(arr[i]>kk.peek()){
+
+        while(!kk.isEmpty()&&arr[i]>kk.peek()){
             kk.pop();
-
-
         }
 
-      
-            kk.push(t.val);
-        
+        kk.push(arr[i]);
+    
+   
     }
 
     else{
-             kk.push(t.val);
+     
 
+ 
+            kk.push(arr[i]);
+        
     }
-    t=t.next;
-  }
 
 
 
-  if(kk.isEmpty()){
-    return null;
-  }
-    Stack<Integer> kj=new Stack<>();
-
-  while(!kk.isEmpty()){
-    kj.push(kk.pop());
-  }
+}
 
 
- ListNode h2=new ListNode(kj.pop());
- ListNode t2=h2;
 
- while(!kj.isEmpty()){
-    ListNode f=new ListNode(kj.pop());
+
+int l=kk.size()-1;
+int m=l;
+
+
+while(!kk.isEmpty()){
+    res[l]=kk.pop();
+    l--;
+}
+
+
+
+
+ListNode h=new ListNode(res[0]);
+ListNode t2=h;
+
+for(int i=1;i<=m;i++){
+    ListNode f=new ListNode(res[i]);
+
     t2.next=f;
     t2=t2.next;
- }
+}
 
 
- return h2;
+return h;
+
+
+
+
+
+
 
 
         
