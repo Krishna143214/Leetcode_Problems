8bbcd6e644726556b6e1aa5ecc36592eb1f615da
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-16 23:19:29 +0530

    Time: 55 ms (46.68%), Space: 99.7 MB (8.59%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,46 +1,66 @@
 import java.util.HashMap;
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        HashMap<Integer,Integer> kk=new HashMap<>();
+int l=0;
 
-        int i=0;
-        int j=0;
-        long c=0;
-        long max=0;
+long res=0;
+long c=0;
+HashMap<Integer,Integer> kk=new HashMap<>();
 
-        while(j<nums.length){
-            if(kk.containsKey(nums[j])){
-                kk.put(nums[j],kk.get(nums[j])+1);
-            }
-            else{
-                kk.put(nums[j],1);
-            }
-            c +=nums[j];
-
-            if((j-i)+1<k){
-                j++;
-            }
-
-            else if((j-i)+1==k){
-                if(kk.size()==k){
-                    if(c>max){
-                        max=c;
-                    }
-                }
-
-                kk.put(nums[i],kk.get(nums[i])-1);
-                if(kk.get(nums[i])==0){
-                    kk.remove(nums[i]);
-                }
-
-                c -=nums[i];
-
-                i++;
-                j++;
-            }
-        }
-
-        return max;
-        
+for(int i=0;i<k;i++){
+    if(kk.containsKey(nums[i])){
+        kk.put(nums[i],kk.get(nums[i])+1);
     }
+    else{
+        kk.put(nums[i],1);
+    }
+
+    c=c+nums[i];
+    
 }
+
+if(kk.size()==k){
+    res=Math.max(c,res);
+}
+
+int j=k;
+
+
+while(j<nums.length){
+
+c=c-nums[l];
+
+kk.put(nums[l],kk.get(nums[l])-1);
+
+if(kk.get(nums[l])<=0){
+    kk.remove(nums[l]);
+}
+l++;
+
+c=c+nums[j];
+
+
+  if(kk.containsKey(nums[j])){
+        kk.put(nums[j],kk.get(nums[j])+1);
+    }
+    else{
+        kk.put(nums[j],1);
+    }
+
+if(kk.size()==k){
+   res=Math.max(c,res);
+}
+
+j++;
+
+}
+
+
+return res;
+
+
+
+
+
+
+    }}
