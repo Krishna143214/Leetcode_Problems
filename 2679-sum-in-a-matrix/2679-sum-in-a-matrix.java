920abb84f1b87f7b02e52fdefdd3f9534ede2a84
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-17 16:13:48 +0530

    Time: 38 ms (10.55%), Space: 81.6 MB (5.2%) - LeetHub

--- 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
+++ 2679-sum-in-a-matrix/2679-sum-in-a-matrix.java
@@ -4,21 +4,25 @@ import java.util.Arrays;
 class Solution {
     public int matrixSum(int[][] nums) {
 
- for(int i=0;i<nums.length;i++){
+for(int i=0;i<nums.length;i++){
     Arrays.sort(nums[i]);
- }
 
- int ans=0;
+} 
 
- for(int i=0;i<nums[0].length;i++){
-    PriorityQueue<Integer> l=new PriorityQueue<>(Collections.reverseOrder());
+int c=0;
+
+for(int i=0;i<nums[0].length;i++){
+    PriorityQueue<Integer> kk=new PriorityQueue<>(Collections.reverseOrder());
     for(int j=0;j<nums.length;j++){
-        l.add(nums[j][i]);
+
+        kk.add(nums[j][i]);
+
     }
-    ans=ans+l.peek();
- }
+
+    c=c+kk.poll();
+}
 
 
- return ans;       
+return c;
     }
 }
