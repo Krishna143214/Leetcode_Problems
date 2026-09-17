3ac1d27a2b312d3a7c4e6a5950aaf9b87c702f0a
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-17 16:04:22 +0530

    Time: 220 ms (26.7%), Space: 136.4 MB (10.86%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.java
@@ -2,42 +2,45 @@ import java.util.PriorityQueue;
 import java.util.HashSet;
 class Solution {
     public long findScore(int[] nums) {
-            HashSet<Integer> ll=new HashSet<>();
-    PriorityQueue<pair> kk=new PriorityQueue<>(
-        (a,b)->{
-            if(a.val!=b.val){
-                return a.val-b.val;
+
+        HashSet<Integer> kk=new HashSet<>();
+
+        PriorityQueue<pair> ll=new PriorityQueue<>(
+
+            (a,b)->{
+                if(a.val != b.val){
+                    return a.val-b.val;
+                }
+
+                return a.ind-b.ind; 
             }
-            return a.ind-b.ind;
+        );
+
+        for(int i=0;i<nums.length;i++){
+            ll.add(new pair(nums[i],i));
         }
-    );
-
-    for(int i=0;i<nums.length;i++){
-        kk.add(new pair(nums[i],i));
-    }
 
 
 
-    long c=0;
+        long c=0;
 
-    while(!kk.isEmpty()){
-        pair g=kk.poll();
+        while(!ll.isEmpty()){
+            pair g=ll.poll();
 
-        if(!ll.contains(g.ind)){
-        c=c+g.val;
+            if(!kk.contains(g.ind)){
+                c=c+g.val;
 
-    
-       ll.add(g.ind);
-       ll.add(g.ind+1);
-       ll.add(g.ind-1);
+                kk.add(g.ind);
+                kk.add(g.ind+1);
+                   kk.add(g.ind-1);
+            }
         }
-        
 
 
-    }
+        return c;
 
 
-    return c;
+
  
 
         
@@ -48,8 +51,8 @@ class pair{
     int val;
     int ind;
 
-   pair(int a,int b){
-    val=a;
-    ind=b;
-   }
+    pair(int a,int b){
+        val=a;
+        ind=b;
+    }
 }
