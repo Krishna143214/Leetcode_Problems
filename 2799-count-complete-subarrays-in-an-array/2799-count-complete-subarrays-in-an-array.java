aad5c7fd17743ce5a3313ccb684e0eafbcc88e73
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-17 16:36:02 +0530

    Time: 237 ms (22.13%), Space: 46.9 MB (71.88%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.java
@@ -1,26 +1,30 @@
 import java.util.HashSet;
 class Solution {
     public int countCompleteSubarrays(int[] nums) {
-        HashSet<Integer> kk=new HashSet<>();
+    HashSet<Integer> kk=new HashSet<>();
 
-        for(int i=0;i<nums.length;i++){
-            kk.add(nums[i]);
-        }
-        int c=0;
+    for(int a:nums){
+        kk.add(a);
+    }
+int c=0;
+    for(int i=0;i<nums.length;i++){
+        HashSet<Integer> ll=new HashSet<>();
+        for(int j=i;j<nums.length;j++){
+            ll.add(nums[j]);
 
-        for(int i=0;i<nums.length;i++){
-            HashSet<Integer> ll=new HashSet<>();
-            for(int j=i;j<nums.length;j++){
-                ll.add(nums[j]);
-                if(ll.size()==kk.size()){
-                    c++;
-                }
-
+            if(ll.size()==kk.size()){
+                c++;
             }
+
+
         }
+    }
 
 
-        return c;
+    return c;
+
+
+   
         
     }
 }
