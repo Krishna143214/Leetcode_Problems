1a669d99b2cda8d7e22d7fb65df969319def5e9d
Krishna Dubey <112738738+Krishna143214@users.noreply.github.com>
2026-09-18 21:05:49 +0530

    Time: 4 ms (12.63%), Space: 47.3 MB (70.74%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,44 +1,54 @@
 class Solution {
+
+    
     public int[] resultsArray(int[] nums, int k) {
 
         if(k==1){
             return nums;
         }
-        
 
-        int res[]=new int[(nums.length-k)+1];
-        
-
-        for(int i=0;i<=nums.length-k;i++){
-            int f=0;
-            int max=0;
-            for(int j=i;j<i+k;j++){
-                
-                if(j<(i+k)-1){
-                    if(nums[j]+1!=nums[j+1]){
-                        f=1;
-                      
-                    }
-                      max=nums[j+1];
-
-                }
-
-            }
-
-
-            if(f==0){
-
-res[i]=max;
-            }
-
-            else{
-                res[i]=-1;
-            }
+        if(nums.length==1){
+            return new int[]{nums[0]};
         }
 
 
-        return res;
+        int arr[]=new int[nums.length-k+1];
+        int l=0;
 
+        
+for(int i=0;i<=nums.length-k;i++){
+    int f=0;
+    int h=-1;
+    for(int j=i;j<i+k-1;j++){
+
+        if(nums[j]+1==nums[j+1]){
+            h=nums[j+1];
+        }
+        else{
+            f=1;
+        }
+
+        
+
+      
+
+   
+
+    }
+
+    if(f==0){
+        arr[l]=h;
+        l++;
+    }
+    else{
+        arr[l]=-1;
+        l++;
+    }
+}
+return arr;
+
+
+  
 
         
         
